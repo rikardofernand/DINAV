e2981359d9e23500a46cef7416a03b075c77ccc8
Ricardo Fernandes (1180099) <rmfernande@gmail.com>
2019-06-15 09:47:53 +0100

    introducao da funcao cinematica direta

--- interpolacao_cubica.m
+++ interpolacao_cubica.m
@@ -2,22 +2,19 @@ function [theta,thetap, thetapp , t] = interpolacao_cubica(thetaini, thetaf,tf)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 global n
-a0=thetaini; %tetha inicial  
+a0=thetaini; %tetha inicial
 a1=0 ;
 a2=3/tf^2*(thetaf-thetaini);
 a3=-2/tf^3 *(thetaf-thetaini);
 
 t=0:tf/n:tf;
-k=1;
 
 for w=1:n+1
-    theta(k)=a0+a1*t(w)+a2*t(w).^2+a3*t(w).^3;
-    thetap(k)=2*a2*t(w)+3*a3*t(w).^2;
-    thetapp(k)=2*a2+6*a3*t(w);
-       k=k+1;
+    theta(w)=a0+a1*t(w)+a2*t(w).^2+a3*t(w).^3;
+    thetap(w)=2*a2*t(w)+3*a3*t(w).^2;
+    thetapp(w)=2*a2+6*a3*t(w);
+    
 end
-
-
 
 
 end
